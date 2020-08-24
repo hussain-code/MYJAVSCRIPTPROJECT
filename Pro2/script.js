6ff6af86e9f372206891331b80eb580776c5e227
hussain-code <hussainkhan079@gmail.com>
2020-08-24 12:59:58 +0500

    z

--- Pro2/script.js
+++ Pro2/script.js
@@ -5,4 +5,10 @@ const total = document.getElementById('total');
 const movieselect = document.getElementById('movie');
 const ticketprice = +movieselect.Value; 
 
-console.log(typeof ticketprice);
+
+// Event listner for click on available seat
+container.addEventListener('click', (e) => {
+    if(e.target.classlist.contains('seat') && !e.target.classlist.contains('Occupied'));{
+        console.log(e.target);
+    }
+})
