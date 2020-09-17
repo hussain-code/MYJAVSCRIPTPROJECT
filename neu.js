7560dab2a92e33368ccd52079f48a5761a296dad
hussain-code <hussainkhan079@gmail.com>
2020-09-17 14:18:15 +0500

    q

--- neu.js
+++ neu.js
@@ -4,7 +4,18 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+//All Function
+function showError(input,message) {
+    const formcontrol = input.parentElement;
+    formControl.className = 'form-control error';
+}
 
-form.addEventListener('submit',function(){
+// This is an event listner for the form on submit
+form.addEventListener('submit',function(e) {
+    e.preventDefault();
+   
+    if (username.value === '' ) {
+        showError(username,'Username is required')
+    }
 
-});
+})
