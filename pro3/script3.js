cea7df7d4fbf2f73fcd4d296947ef84cdae8b13c
hussain-code <hussainkhan079@gmail.com>
2020-09-25 11:39:39 +0500

    Completed work JS

---
+++ pro3/script3.js
@@ -0,0 +1,27 @@
+//Get dom element
+const toggle =document.getElementById('tiggke');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const model= document.getElementById('model');
+
+//Add Event  Listeners
+//1.toglle the NAV
+toggle.addEventListener('click',() => 
+    document.body.classList.toggle('show-nav')
+);
+
+// 2. show the model
+open.addEventListener('click', ()  => 
+model.classList.add('show-model') 
+);
+
+//3. close the model
+close.addEventListener('click', ()  => 
+model.classList.remove('show-model')
+);
+
+//4. close the model on click outside model
+
+window.addEventListener('click',e => 
+    e.target === model ? model.classList.remove('show-model') : false 
+    );
