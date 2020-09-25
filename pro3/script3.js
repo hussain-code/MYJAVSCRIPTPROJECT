0feb75321eeb683507a29d581330321e194f1763
hussain-code <hussainkhan079@gmail.com>
2020-09-25 11:51:07 +0500

    Completed

--- pro3/script3.js
+++ pro3/script3.js
@@ -1,5 +1,5 @@
 //Get dom element
-const toggle =document.getElementById('tiggke');
+const toggle =document.getElementById('toggle');
 const close = document.getElementById('close');
 const open = document.getElementById('open');
 const model= document.getElementById('model');
