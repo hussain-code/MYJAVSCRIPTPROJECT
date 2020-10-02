6dcb0a3cddbafde7c4d5f11cc6dca394ef234ab6
hussain-code <hussainkhan079@gmail.com>
2020-10-02 10:39:31 +0500

    Update script4.js

--- pro3/pro4/script4.js
+++ pro3/pro4/script4.js
@@ -1,25 +1,38 @@
 //Grab dom elements form Html Elements
 const word = document.getElementById('word');
-const wrongletters = documnet.getElementById('wrongletters');
-const message = documnet.getElementById('win-lose');
+const wrongletters = document.getElementById('wrong-letters');
+const message = document.getElementById('win-lose');
 const popup = document.getElementById('popup-container');
 const restartButton = document.getElementById('restart');
 const notificatin= document.getElementById('slider-container');
 
 const hangmanParts = document.querySelectorAll('.hangman-part');
 
-const wordpool = ['javascript','Computer','Facebook','hangman'];
+const wordpool = ['javascript','computer','facebook','hangman'];
 //selecting a word random from pool
 let selectedword = wordpool[Math.random() * wordpool.length];
 //array to classify the  input of the letter
-const correctletters =[];
+const correctletters =['a','o','i','e'];
 const incorrectletters =[];
 // functin to display the word on the screen
 
 function displayselectedword() {
-  word.innerHTML = '
-  s{selectedword} 
-            ';
+  word.innerHTML= `
+    ${selectedword
+      .split ('')
+      . map(
+         letter => `
+         <span class="letter">
+          ${correctletters.includes(letter) ? letter : '' }
+         </span>
+         `
+      )
+      .join('')
+    } 
+  `;
+console.log(word.innerText);
+
 }
+
 
 displayselectedword();
